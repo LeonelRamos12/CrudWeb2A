fc1030a0e3bccbe97f5fde353766a174b0c10e59
Leonel <20240005@ricaldone.edu.sv>
2025-06-05 15:43:44 -0600

    Funcion para agregar casi terminada

--- JS/API.js
+++ JS/API.js
@@ -35,3 +35,22 @@ function MostrarDatosEmpleados(datos) {
 
 GetEmployees();
 
+//proceso para agregar un nuevo integrante
+const modal = document.getElementById("mdAgregar") //cuadro de dialogo
+const btnAgregar = document.getElementById("btnAgregar")//boton para agregar
+const btnCerrar = document.getElementById("btnCerrar")//boton para cerrar
+
+btnAgregar.addEventListener("click", ()=>{
+    modal.showModal();//se abrira el formulario para agregar nuevo integrante
+});
+
+btnCerrar.addEventListener("click", ()=>{
+  modal.close();
+})
+
+
+//Agregar nuevo integrante 
+document.getElementById("frmAgregar").addEventListener("submit", async e => {
+    e.preventDefault();//"e" representa a submit y evita que el formulario se envie de un solo.
+    
+});
